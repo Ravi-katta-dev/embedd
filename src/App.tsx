a2dd9e9fd83f0bda313934b758c448523996a4fd
[dyad] <kattaravikumar009l.sse@saveetha.com>
2026-05-10 14:50:51 +0530

    [dyad] Fixed App.tsx export error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
-// ... (existing routes)
-// Add new routes for content tabs (no change needed as tabs are client-side)
-<Route path="/module/:moduleId/topic/:topicId" element={<TopicView />} />
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import ModuleDetail from './pages/ModuleDetail';
+import TopicView from './pages/TopicView';
+import Certifications from './pages/Certifications';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/module/:moduleId" element={<ModuleDetail />} />
+        <Route path="/module/:moduleId/topic/:topicId" element={<TopicView />} />
+        <Route path="/certifications" element={<Certifications />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
